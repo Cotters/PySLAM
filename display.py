2f3584969851964e84e4f2c3257b800b697b03d8
cotters <cotterelljra@gmail.com>
2019-04-19 10:58:08 +0100

    feature extractor

--- display.py
+++ display.py
@@ -17,19 +17,9 @@ class Display(object):
 		for event in events:
 			if event.type == sdl2.SDL_QUIT:
 				exit(0)
+
 		surf = sdl2.ext.pixels3d(self.window.get_surface())
 		surf[:, :, 0:3] = img.swapaxes(0,1)
+
 		self.window.refresh()
 
-if __name__ == "__main__":
-	cap = cv2.VideoCapture("test.mp4")
-
-	while cap.isOpened():
-		ret, frame = cap.read()
-		if ret == True:
-			process_frame(frame)
-		else:
-			break
-
-#cap.release()
-#cv2.destroyAllWindows()
