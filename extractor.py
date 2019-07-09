4a0e9722c5cb9578902bb2b65165c350075c2d3e
cotters <cotterelljra@gmail.com>
2019-07-09 20:03:01 +0100

    normalise coords

--- extractor.py
+++ extractor.py
@@ -3,16 +3,27 @@ import numpy as np
 
 from skimage.measure import ransac
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
 
-#from skimage.measure import ransac
-#from skimage.transform import FundamentalMatrixTransform
+def add_ones(x):
+	return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
+
 
 class Extractor(object):
-	def __init__(self):
+	def __init__(self, K):
 		self.orb = cv2.ORB_create()
 		self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 		self.last = None
-	
+		self.K = K
+		self.Kinv = np.linalg.inv(K)
+
+	def normalise(self, pts):
+		return np.dot(self.Kinv, add_ones(pts).T).T[:,0:2]
+
+	def denormalise(self, pt):
+		ret = np.dot(self.K, [pt[0], pt[1], 1.0])
+		return int(round(ret[0])), int(round(ret[1]))
+
 	def extract(self, img):
 		# detection
 		feats = cv2.goodFeaturesToTrack(np.mean(img, axis=2).astype(np.uint8), 3000, qualityLevel=0.01, minDistance=3)
@@ -20,7 +31,7 @@ class Extractor(object):
 		# extraction
 		kps = [cv2.KeyPoint(x=f[0][0], y=f[0][1], _size=20) for f in feats]
 		kps, des = self.orb.compute(img, kps)
-		
+	
 		# matching
 		ret = []
 		if self.last is not None:
@@ -30,17 +41,24 @@ class Extractor(object):
 					kp1 = kps[m.queryIdx].pt
 					kp2 = self.last['kps'][m.trainIdx].pt
 					ret.append((kp1,kp2))
-
+		
+		
 		# filter
 		if len(ret) > 0:
 			ret = np.array(ret)
+			# normalise coords: subtract to move to 0	
+			ret[:, 0, :] = self.normalise(ret[:, 0, :])
+			ret[:, 1, :] = self.normalise(ret[:, 1, :])
+
 			model, inliers = ransac((ret[:,0], ret[:,1]),
+															#EssentialMatrixTransform,
 															FundamentalMatrixTransform,
 															min_samples=8,
 															residual_threshold=1,
 															max_trials=100)
 			ret = ret[inliers]
-	
+			s, v, d = np.linalg.svd(model.params)
+			#print(v)
 		# return
 		self.last = {'kps': kps, 'des': des}
 		return ret
--- slam.py
+++ slam.py
@@ -3,21 +3,30 @@ import time
 import cv2
 from display import Display
 from extractor import Extractor
+import numpy as np
 
 W = 1920//2
 H = 1080//2
-
 disp = Display(W,H)
 
-fe = Extractor()
+F = 1
+K = np.array([[F,0,W//2],[0,F,H//2],[0,0,1]])
+# using W//3.9 and H//1.2 works
+#K = np.array([[F,0,0],[0,F,0],[0,0,1]])
+
+fe = Extractor(K)
 
 def process_frame(img):
 	img = cv2.resize(img, (W,H))
 	matches = fe.extract(img)
-
+	
 	for pt1, pt2 in matches:
-		u1,v1 = map(lambda x: int(round(x)), pt1)
-		u2,v2 = map(lambda x: int(round(x)), pt2)
+		# denormalise for display
+		u1,v1 = fe.denormalise(pt1)
+		u2,v2 = fe.denormalise(pt2)
+		#u1,v1 = map(lambda x: int(round(x)), pt1)
+		#u2,v2 = map(lambda x: int(round(x)), pt2)
+
 		cv2.circle(img, (u1,v1), color=(0,255,0), radius=3)
 		cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))
 
